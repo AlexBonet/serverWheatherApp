57f1a634f15d15f77c693747be67f178ad44d7ee
Alex Bonet <abonetm@ieslavereda.es>
2022-05-06 17:15:22 +0200

    0

--- src/main/java/es/ieslaverda/server/API.java
+++ src/main/java/es/ieslaverda/server/API.java
@@ -1,6 +1,6 @@
 package es.ieslaverda.server;
 
-public class API {
+public class API  {
     public static class Routes{
         public static final String ALL_CITIES = "/cities";
         public static final String CITY_ADD = "/cadd";
